60dba725124fdde5f0f8d16104ecb92b9be2d20b
ak8006181716 <153820262+ak8006181716@users.noreply.github.com>
2025-06-16 17:05:11 +0530

    add refresh token functionality and improve JWT verification

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -3,6 +3,8 @@ import ApiError from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 import { uploadCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import jwt from "jsonwebtoken";
+
 
 const generateAccessAndRefreshToken = async(userId)=>{
 
@@ -76,14 +78,16 @@ const registerUser = asyncHandler(async (req, res) => {
 });
 
 const loginUser = asyncHandler(async (req, res)=>{
+const { password, email, username } = req.body; 
 
-const {email, username, password} =req.body;
 
-
-if(!email || !username ) throw new ApiError(404, "username or email is required");
+if(!(email || username) ) throw new ApiError(404, "username or email is required");
 
   const user= await User.findOne({
-    $or:[username,email]
+    $or: [
+    { email: email },
+    { username: username }
+  ]
   })
   if(!user) throw new ApiError(404,"user not found");
 
@@ -93,7 +97,7 @@ if(!email || !username ) throw new ApiError(404, "username or email is required"
 
   const {accessToken, refreshToken} = await generateAccessAndRefreshToken(user._id)
 
-  const logedInUser = User.findById(user._id).select("-password -refreshToken");
+  const logedInUser = await User.findById(user._id).select("-password -refreshToken");
 
   const options ={
     httpOnly:true,
@@ -105,7 +109,11 @@ if(!email || !username ) throw new ApiError(404, "username or email is required"
   .cookie("accessToken",accessToken,options)
   .cookie("refreshToken",refreshToken,options)
   .json( new ApiResponse (201,{
-    user:logedInUser,accessToken,refreshToken
+    user:
+    logedInUser,
+    accessToken,
+    refreshToken
+    
   },
   "user loged in successfully"
 ))
@@ -132,9 +140,48 @@ const logoutUser =asyncHandler(async (req ,res)=>{
   .json(new ApiResponse(200, null, "User logged out successfully"));
 
 })
+
+const refreshAccessToken = asyncHandler(async (req, res) => {
+ 
+  const incomingRefreshToken= req.cookies.refreshToken || req.body.refreshToken;
+
+
+ if(!incomingRefreshToken) throw new ApiError(401,"unauthorized access, refresh token is required");
+ try {
+   const decodedToken = jwt.verify(incomingRefreshToken, process.env.REFRESH_TOKEN_SECRET);
+ const user = await User.findById(decodedToken?._id);
+ if(!user) throw new ApiError(401,"unauthorized access, refresh token is invalid");
+ 
+ if(incomingRefreshToken!==user?.refreshToken) throw new ApiError(401,"unauthorized access, refresh token is invalid");
+ 
+ 
+ const options={
+   httpOnly:true,
+   secure:true
+ }
+ 
+ const { accessToken, newRefreshToken } = await generateAccessAndRefreshToken(user._id);
+ 
+ return res
+ .status(201)
+ .cookie("accessToken",accessToken)
+ .cookie("refreshToken",newRefreshToken)
+ .json(
+   new ApiResponse(201,
+     {accessToken,refreshToken: newRefreshToken},
+     "Access Token successfully updated")
+ )
+ } catch (error) {
+    throw new ApiError(401, error?.massage || "something went wrong in token refreshing")
+ }
+})
+
+
+
 export 
 { 
   registerUser,
   loginUser,
   logoutUser, 
+  refreshAccessToken
 };
--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -3,11 +3,14 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { User } from "../models/user.model.js";
 import jwt from "jsonwebtoken";
 
-
+// Middleware to verify JWT and attach user to request object
+// This middleware checks for a valid JWT in the request headers or cookies
 export const verifyJWT =asyncHandler(async (req, _, next)=>{
   try {
+    // Check for token in cookies or Authorization header
+     
      const token= req.cookies?.accessToken ||req.header("Authorization")?.replace("Bearer ","");
-     if(!token) throw new ApiError(401,"Unotherized access");
+     if(!token) throw new ApiError(401,"Unotherized access");// If token is not found, throw an error
   
   
      const decodedtoken = jwt.verify(token,process.env.ACCESS_TOKEN_SECRET);
